aa7e23b5d77b8ec39af06fdf862fbc2081f047e1
Slaanaroth <corentin.marionneau@gmail.com>
2017-12-06 20:48:01 +0100

    Commented java file

--- queries/MultigraphQuery.java
+++ queries/MultigraphQuery.java
@@ -3,8 +3,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
-
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.Query;
@@ -19,21 +17,26 @@ import org.apache.jena.util.FileManager;
 
 public class MultigraphQuery {
 
+	// Path to both graphs
 	public static String graph = "/home/slaanaroth/Documents/TARQL/WebSemantic/rdf/graphFinal.ttl";
 	public static String their_graph = "/home/slaanaroth/Documents/TARQL/WebSemantic/rdf/their_graph.ttl";
 	
 	
 	public static void main(String[] args) throws IOException {
 		
+		// Creating a model for each graph
 		Model data = FileManager.get().loadModel(graph,"TTL");
 		Model their_data = FileManager.get().loadModel(their_graph,"TTL");
+		
+		// Creating a dataset and adding both models as named graphs
 		Dataset dataset = DatasetFactory.create() ;
 		dataset.addNamedModel("our_graph",data) ;
 		dataset.addNamedModel("their_graph", their_data) ;
 		
-		
+		// Reading the .sparql file to retrieve the query
 		String queryString = readFile("multigraph_query.sparql", StandardCharsets.UTF_8);
 		
+		// Creating the query, executing it and saving the result in a variable
 		Query query = QueryFactory.create(queryString) ;
 		ResultSet results = null;
 		try (QueryExecution qexec = QueryExecutionFactory.create(queryString, dataset)) {
@@ -41,10 +44,12 @@ public class MultigraphQuery {
 		    results = ResultSetFactory.copyResults(results) ;
 		}
 		
+		// Formatting the result and printing it in the default console output
 		ResultSetFormatter.out(System.out, results, query) ;
 
 	}
 	
+	// A method to return the content of a file as a String
 	static String readFile(String path, Charset encoding) 
 			  throws IOException 
 			{
